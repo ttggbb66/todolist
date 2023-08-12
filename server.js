3f815b483876b7f2ae6b54cbc2df543c10b6c70b
Jiaqian Li <nicoleli1231@gmail.com>
2023-08-11 23:00:24 -0400

    install

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Set up MongoDB connection
+mongoose.connect('mongodb://localhost/mean_todo_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
